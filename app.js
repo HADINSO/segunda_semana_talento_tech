b8f336686db97a9ef98c251d3f12edd42e7ce2f7
HADINSO <hadisferrin@gmail.com>
2025-09-16 14:11:50 -0500

    Dia 16

---
+++ app.js
@@ -0,0 +1,44 @@
+/*
+
+//Areglos 
+let datosPersonales = [
+  "HADINSO HINESTROA",
+  "1131045489",
+  "3146642251",
+  26,
+  "Piñal de medrano, tercera calle",
+  1.73,
+];
+
+//Metodos para trabajar con listas
+datosPersonales.push("Hombre") //Agrega un dato al final
+datosPersonales.unshift(1) //Agrega un dato al principio
+datosPersonales.pop(); //elimina el ultimo elemento
+datosPersonales.shift(); //elimina al primer elemento
+datosPersonales[2] = "3146642252" //Modificar un valor
+
+console.log(datosPersonales);
+
+//Tuplas
+
+const tupla = ["manzana", "pera", "fresa"]
+console.log(tupla[0])
+console.log(tupla.includes("manzana"))
+
+
+//Diccionarios
+
+let datosPersonales = {
+  Nombres: "LISETH DAHIANA",
+  Apellidos: "PALACIO ANDRADE",
+  Telefono: "312342243",
+  Sexo: "Masculino",
+};
+
+console.log(Object.values(datosPersonales))
+
+
+var nombre = prompt("Propocione su nombre: ")
+console.log(nombre)
+*/
+//Condicionales
